80e31c46ee5491cc712256b76e7ec8b495f6ffc8
Daisy Barbanel <daisybarbanel@gmail.com>
2018-11-08 16:49:48 -0600

    p3 starter code

---
+++ pintos-p3/src/userprog/syscall.h
@@ -0,0 +1,27 @@
+#ifndef USERPROG_SYSCALL_H
+#define USERPROG_SYSCALL_H
+
+#include "lib/stdbool.h"
+#include "process.h"
+#include <list.h>
+
+/* Note for Raphael: create, remove */
+
+/* Process identifier. */
+void syscall_init (void);
+int sys_write (int handle, void *usrc_, unsigned size);
+int sys_create (const char *file, unsigned initial_size);
+int sys_open (const char *file);
+void sys_exit (int status);
+void sys_halt (void) NO_RETURN;
+pid_t sys_exec (const char *file);
+int sys_wait (pid_t);
+int get_handle (void);
+void sys_close (int handle);
+int sys_remove (const char *file);
+int sys_filesize (int handle);
+int sys_read (int handle, void *buffer, unsigned size);
+unsigned tell (int handle);
+void seek (int handle, unsigned position);
+
+#endif /* userprog/syscall.h */
